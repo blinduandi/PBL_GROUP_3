8cd08101c03be44665b446e83ef54567b6407350
caramisca <caramisca2@gmail.com>
2024-04-01 14:10:27 +0300

    Homepage frontend

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.min.css" integrity="sha512-i1b/nzkVo97VN5WbEtaPebBG8REvjWeqNclJ6AItj7msdVcaveKrlIIByDpvjk5nwHjXkIqGZscVxOrTb9tsMA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="style/style.css">
+        <script src="js/index.js"></script>
+        <title>My First Site</title>
+    </head>
+    <body>
+<div class="container-fluid" style="background-color: #fff;padding:0 ;">
+        <div class="container navh overflow-hidden text-center" >
+            <div class="row align-items-end">
+                <div class="col-6">
+                    <a href="/" class="logo-text">QuickRoute</a>
+                </div>
+
+                <div class="col-6"> 
+                    <div class="row " >
+                    <div class="col-2"><a href="/" class="link-h">Acasa</a></div>
+                    <div class="col-2"><a href="/meniu" class="link-h">Meniu</a></div>
+                    <div class="col-2"><a href="/news" class="link-h">News</a></div>
+                    <div class="col-2"><a href="/livrare" class="link-h">Livrare</a></div>
+                    <div class="col-2"><a href="/contacte" class="link-h">Contacte</a></div>
+                    <div class="col-2 cart-h" > <i class="fa-solid fa-cart-shopping"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+        <section id="img-underh">
+    <img class="kebab" src="https://linde-stories.com/wp-content/uploads/2021/03/shutterstock_1151970542.jpg" alt="" srcset="">
+        </section>
+        <nav>Navbar</nav>
+        <section>Ion</section>
+        <section>is</section>
+        <section>nigga</section>
+        <footer>a footer</footer>
+        <div class="logo"></div>
+        <button><i class="fa-brands fa-twitter"></i></button>
+
+
+        
+    </body>
+</html>
